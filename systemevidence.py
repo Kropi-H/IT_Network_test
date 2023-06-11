35050df86964f6c606bbf81ac57946c84e46692f
Kropi-H <kropacek.h@gmail.com>
2023-06-11 14:11:27 +0200

    využití docstringu

--- systemevidence.py
+++ systemevidence.py
@@ -1,7 +1,10 @@
 from pojisteny import Pojisteny
 class SystemEvidence():
+    """
+    Třída reprezentuje vytváření a evidování pojištěnců
+    """
 
-    # Fake pocatecni stav uzivatelu
+    #Fake list pojistenců. Jen aby tam nějaký na začátku byl ...
     pojistenci = [
         ["Marcel", "Malý", 68, "666333111", 15]
     ]
@@ -9,17 +12,32 @@ class SystemEvidence():
     def __init__(self):
         pass
 
-    # Vytvoření pojisteného a připíchnutí do listu pojistenci
+
     def vytvor_pojisteneho(self, jmeno, prijmeni, vek, telefon):
+        """
+        Vytvoření pojistného vložením (jmeno, prijmeni,vek, telefon)
+        :param: jmeno
+        :param: prijmeni
+        :param: vek
+        :param: telefon
+        Navýšíme poslední číslo id z pojistenci (zavoláním metody vrat_posledni_id_uzivatele) a navýšíme o jedna
+        :return: Vytvoření pojisteného a jeho přidání do listu pojistenci
+        """
         pojisteny = [jmeno, prijmeni, vek, telefon, self.vrat_posledni_id_uzivatele() + 1]
         SystemEvidence.pojistenci.append(pojisteny)
 
     def vrat_posledni_id_uzivatele(self):
+        """
+        Není to nezbytné, jenom mě zajímala implementace - (databáze by se o to postarala sama)
+        :return:Vrací poslední id uživatele.
+        """
         pojistenec_id = SystemEvidence.pojistenci[-1][-1]
         return pojistenec_id
 
-
     def vypis_pojistencu(self):
+        """
+        :return:Vypisuje pojištěnce z listu pojistenci do zarovnané tabulky
+        """
         column_widths = [max(len(str(item)) for item in column) for column in zip(*SystemEvidence.pojistenci)]
         for jmeno in SystemEvidence.pojistenci:
             for i, width in zip(jmeno, column_widths[:-1]):
@@ -27,6 +45,13 @@ class SystemEvidence():
             print()
 
     def vrat_uzivatele_podle_jmena_prijmeni(self,jmeno,prijmeni):
+        """
+        Vrací uzivatele, kterého hledáme v listu pojistenci podle jmena a prijmeni.
+        Hledáni jsou podle indexů pojistenec[0] a pojistenec[1]
+        :param jmeno:
+        :param prijmeni:
+        :return: String uživatele nebo string o neúspěšném hledání
+        """
         self.__jmeno = jmeno
         self.__prijmeni = prijmeni
         for pojistenec in SystemEvidence.pojistenci:
